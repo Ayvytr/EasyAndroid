35df80bc1c34847663b64c10bff05cde36a939ee
Ayvytr <ayvytr@outlook.com>
2017-04-27 09:28:32 +0800

    1.升级上传版本1.7.8

--- EasyAndroid/src/main/java/com/ayvytr/easyandroid/tools/withcontext/Res.java
+++ EasyAndroid/src/main/java/com/ayvytr/easyandroid/tools/withcontext/Res.java
@@ -16,7 +16,7 @@ import com.ayvytr.easyandroid.exception.UnsupportedInitializationException;
  * 获取资源中的Drawable，String，dimension，color, Configuration(简化ResTool类名为Res，调用更方便).
  *
  * @author Ayvytr <a href="https://github.com/Ayvytr" target="_blank">'s GitHub</a>
- * @since 1.7.9
+ * @since 1.7.8
  */
 
 public class Res
