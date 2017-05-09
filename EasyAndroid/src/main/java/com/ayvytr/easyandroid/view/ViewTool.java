fc81d8178bc92e79b084c34212a943980ac7f21d
Ayvytr <ayvytr@outlook.com>
2017-05-09 10:16:01 +0800

    1.添加ViewTool类 2.升级版本

---
+++ EasyAndroid/src/main/java/com/ayvytr/easyandroid/view/ViewTool.java
@@ -0,0 +1,124 @@
+package com.ayvytr.easyandroid.view;
+
+import android.support.annotation.LayoutRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ayvytr.easyandroid.Easy;
+import com.ayvytr.easyandroid.exception.UnsupportedInitializationException;
+
+/**
+ * View布局操作类，包括设置可见/隐藏，获取/取消焦点，调用 {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+ * 加载布局，以及最重要的 {@link #inflate2(int, ViewGroup)}，专门为RecyclerView.onCreateViewHolder准备.
+ *
+ * @author Ayvytr <a href="https://github.com/Ayvytr" target="_blank">'s GitHub</a>
+ * @since 1.8.1.2
+ */
+
+public class ViewTool
+{
+    private ViewTool()
+    {
+        throw new UnsupportedInitializationException();
+    }
+
+    /**
+     * 显示view
+     *
+     * @param view 目标View
+     */
+    public static void show(View view)
+    {
+        view.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 隐藏view
+     *
+     * @param view 目标View
+     */
+    public static void hide(View view)
+    {
+        view.setVisibility(View.GONE);
+    }
+
+    /**
+     * 设置view为invisible
+     *
+     * @param view 目标View
+     */
+    public static void invisible(View view)
+    {
+        view.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * 获取view焦点
+     *
+     * @param view 目标View
+     */
+    public static void requestFocus(View view)
+    {
+        view.requestFocus();
+    }
+
+    /**
+     * 取消view焦点
+     *
+     * @param view 目标View
+     */
+    public static void clearFocus(View view)
+    {
+        view.clearFocus();
+    }
+
+    /**
+     * 加载布局并返回，详情请看{@link LayoutInflater#inflate(int, ViewGroup)}.
+     *
+     * @param id 布局id
+     * @return 加载的布局
+     */
+    public static View inflate(@LayoutRes int id)
+    {
+        return inflate(id, null);
+    }
+
+    /**
+     * 加载布局并返回，详情请看{@link LayoutInflater#inflate(int, ViewGroup)}.
+     *
+     * @param id     布局id
+     * @param parent 父布局
+     * @return 加载的布局
+     */
+    public static View inflate(@LayoutRes int id, ViewGroup parent)
+    {
+        return inflate(id, parent, parent != null);
+    }
+
+    /**
+     * 加载布局并返回，详情请看{@link LayoutInflater#inflate(int, ViewGroup, boolean)}.
+     *
+     * @param id             布局id
+     * @param parent         父布局
+     * @param attachToParent 是否给加载的布局文件指定父布局
+     * @return 加载的布局
+     */
+    public static View inflate(@LayoutRes int id, ViewGroup parent, boolean attachToParent)
+    {
+        return LayoutInflater.from(Easy.getContext()).inflate(id, parent, attachToParent);
+    }
+
+    /**
+     * 加载布局并返回，专为RecyclerView提供，不给布局文件指定父布局，
+     * 详情请看{@link LayoutInflater#inflate(int, ViewGroup, boolean)}.
+     *
+     * @param id     布局id
+     * @param parent 父布局
+     * @return 加载的布局
+     */
+    public static View inflate2(@LayoutRes int id, ViewGroup parent)
+    {
+        return inflate(id, parent, false);
+    }
+}
