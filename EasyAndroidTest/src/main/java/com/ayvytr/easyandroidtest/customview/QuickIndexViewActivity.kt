c7563fc7783f7b4b64d73f5d6a638f7bb3c3abce
ayvytr <ayvytr@outlook.com>
2017-06-01 18:13:51 +0800

    1.修改QuickIndexViewActivity，添加QuickIndexView 测试方法：修改背景色，是否显示Toast，更改Toast字体颜色，更改Toast背景，更改Toast宽度，更改Toast高度

--- EasyAndroidTest/src/main/java/com/ayvytr/easyandroidtest/customview/QuickIndexViewActivity.kt
+++ EasyAndroidTest/src/main/java/com/ayvytr/easyandroidtest/customview/QuickIndexViewActivity.kt
@@ -1,12 +1,15 @@
 package com.ayvytr.easyandroidtest.customview
 
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ayvytr.easyandroid.tools.Colors
+import com.ayvytr.easyandroid.tools.withcontext.DensityTool
 import com.ayvytr.easyandroid.tools.withcontext.Res
 import com.ayvytr.easyandroidtest.R
 import kotlinx.android.synthetic.main.activity_quick_index_view.*
+import org.jetbrains.anko.toast
 import java.util.*
 
 class QuickIndexViewActivity : AppCompatActivity()
@@ -35,6 +38,7 @@ class QuickIndexViewActivity : AppCompatActivity()
             return Res.getDrawable(drawables[i])
         }
 
+
     fun initView()
     {
         quickIndexView?.setOnLetterChangeListener { position, text, quickIndexView -> }
@@ -48,9 +52,40 @@ class QuickIndexViewActivity : AppCompatActivity()
         btnBottomDrawable.setOnClickListener {
             quickIndexView?.bottomDrawable = randomDrawable
         }
+        btnBackground.setOnClickListener {
+            quickIndexView.setBackgroundColor(randomColor)
+        }
+        btnShowToast.setOnClickListener {
+            quickIndexView.isShowToast = !quickIndexView.isShowToast
+            //这里有问题，不能在屏幕上同时显示多个Toast
+            toast("已经更改了，触摸字母索引试试吧!")
+        }
+        btnToastTextColor.setOnClickListener {
+            quickIndexView.quickTextColor = randomColor
+        }
+        btnToastBackground.setOnClickListener {
+            quickIndexView.quickBackground = ColorDrawable(randomColor)
+        }
+        btnToastWidth.setOnClickListener {
+            quickIndexView.quickWidth = randomToastPx
+        }
+        btnToastHeight.setOnClickListener {
+            quickIndexView.quickHeight = randomToastPx
+        }
     }
 
     private val randomColor: Int
         get() = Colors.rgb(random.nextInt(0xff), random.nextInt(0xff), random.nextInt(0xff))
 
+    private val randomToastPx: Int
+        get()
+        {
+            var i = random.nextInt(1000)
+            while (i < DensityTool.dp2px(50))
+            {
+                i = random.nextInt(1000)
+            }
+
+            return i
+        }
 }
